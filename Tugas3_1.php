c0dcc50b7cd5f69c39be2cff35a4491abffde7a2
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 10:01:12 +0700

    Add files via upload

---
+++ Tugas3_1.php
@@ -0,0 +1,10 @@
+<?php
+$jarak = 360;
+$speed = 90;
+$waktu = ($jarak/$speed)*60;
+
+echo "Jarak kota A-Z = $jarak Km <br>";
+echo "Kecepatan motor = $speed Km/jam <br>";
+echo "Waktu tempuh = ? <br>";
+echo "Jawaban : $waktu Menit";
+?>
